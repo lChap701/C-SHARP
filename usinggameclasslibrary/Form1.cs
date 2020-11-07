8d8c6b4152b0323a2fb0d79bf39f83d3b3177c25
lChap701 <lucas.chapman@stu.indianhills.edu>
2020-11-06 19:33:18 -0600

    Class library example

---
+++ usinggameclasslibrary/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameClasses;
+
+namespace UsingGameClassLibrary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Game g = new Game();
+
+            NameTable n = new NameTable();
+        }
+    }
+}
