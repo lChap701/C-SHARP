43409a626894b76c4ce9d7b390a7f9c9446e0ba5
lChap701 <lucas.chapman@stu.indianhills.edu>
2020-11-06 19:35:44 -0600

    Overloading operators and CompareTo() example

---
+++ CarDemo/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDemo
+{
+    public class Car : IComparable
+    {
+        public string Model { get; set; }
+        public double MPG { get; set; }
+
+        public Car(string model, double mpg)
+        {
+            Model = model;
+            MPG = mpg;
+        }
+
+        public Car(string model)
+        {
+            Model = model;
+            MPG = 20;
+        }
+
+        public static Car operator ++(Car c)
+        {
+            ++c.MPG;
+            return c;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return MPG.CompareTo(obj);
+            /* 
+             MPG.CompareTo(obj) is the same as writing:
+
+            Car temp = (Car)obj;
+            if (this.MPG > temp.MPG) {
+                return 1;
+            } 
+            else if (this.MPG < temp.MPG) {
+                return -1;
+            } 
+            else {
+                return 0;
+            }
+            */
+        }
+    }
+}
---
+++ CarDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            cars.Add(new Car("Wrangler"));
+            cars.Add(new Car("Magnum", 25));
+
+            WriteLine(cars[0].Model + " " + cars[0].MPG);
+            cars[0] = (cars[0]++);
+            WriteLine(cars[0].Model + " " + cars[0].MPG);
+            
+            WriteLine(cars[1].Model + " " + cars[1].MPG);
+            cars[1] = (cars[1]++);
+            WriteLine(cars[1].Model + " " + cars[1].MPG);
+            ReadLine();
+        }
+    }
+}
